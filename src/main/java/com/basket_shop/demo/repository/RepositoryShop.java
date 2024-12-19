2af346e172c8f95b11b113f6d1720737300708bd
Евгений <Safronovbr@mail.ru>
2024-12-20 01:50:30 +0300

    finished

--- src/main/java/com/basket_shop/demo/repository/RepositoryShop.java
+++ src/main/java/com/basket_shop/demo/repository/RepositoryShop.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
+
 @Scope(value = "session",proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Component
 public class RepositoryShop {
-    private final Collection<Integer> BAG = new HashSet<>();
+    private final List<Integer> BAG = new ArrayList<>();
 
     public void add(List<Integer> id) {
         BAG.addAll(id);
